94a7b940033b07a98986197096a37d8532426971
MrDogeBro <35872177+aedanbricker@users.noreply.github.com>
2021-05-21 21:38:26 -0400

    Update for v1.1.0 release

--- Formula/quicknav.rb
+++ Formula/quicknav.rb
@@ -4,9 +4,9 @@
 class Quicknav < Formula
   desc "Quicknav is a command line tool that allows you to easily jump to specific locations in your filesystem from a simple command. Quicknav is built for terminal navigation on Unix operating systems."
   homepage "https://github.com/MrDogeBro/quicknav"
-  url "https://github.com/MrDogeBro/quicknav/archive/v1.0.2.tar.gz"
-  sha256 "dd2bfdb47997a9e5609d69fbecbaea51eee605527fcba1ad27546df2ffa15342"
-  version "1.0.2"
+  url "https://github.com/MrDogeBro/quicknav/archive/v1.1.0.tar.gz"
+  sha256 "af0dfdc00d776fec258ea6c8aa997824485c729a18c65ff93ebb3fb91ba5cfd1"
+  version "1.1.0"
   depends_on "rust" => :build
 
   def install
