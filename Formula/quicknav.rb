4656a375397987571d92b74b56f8fe452800bbec
MrDogeBro <35872177+35872177+aedanbricker@users.noreply.github.com>
2021-06-26 21:21:00 -0400

    Fix incorrect sha256

--- Formula/quicknav.rb
+++ Formula/quicknav.rb
@@ -5,7 +5,7 @@ class Quicknav < Formula
   desc "Quicknav is a command line tool that allows you to easily jump to specific locations in your filesystem from a simple command. Quicknav is built for terminal navigation on Unix operating systems."
   homepage "https://github.com/MrDogeBro/quicknav"
   url "https://github.com/MrDogeBro/quicknav/archive/v1.1.1.tar.gz"
-  sha256 "79be6e58c426cdcad8ee844743f5398c638f40f6504644094f9ee0b72297ce2we"
+  sha256 "79be6e58c426cdcad8ee844743f5398c638f40f6504644094f9ee0b72297ce2e"
   version "1.1.1"
   depends_on "rust" => :build
 
